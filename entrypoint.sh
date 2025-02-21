ec777f79d555a226e57f968ab8c0d6d5dece30ca
Ashwin Kumar Yadav <ashwin.y@aftershoot.com>
2025-02-21 11:16:07 +0530

    fix tailscale binary path

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,11 +7,11 @@ sshd -D &
 # Start Tailscale in the background
 mkdir -p /var/run/tailscale
 echo "Starting tailscale daemon..."
-./tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock --tun=userspace-networking --socks5-server=localhost:1055 --outbound-http-proxy-listen=localhost:1055 &
+/etc/tailscale.d/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock --tun=userspace-networking --socks5-server=localhost:1055 --outbound-http-proxy-listen=localhost:1055 &
 
 # Wait until Tailscale is up
 echo "Waiting for tailscale to be up..."
-until ./tailscale up --login-server="${TAILSCALE_LOGIN_SERVER}" --authkey="${TAILSCALE_AUTHKEY}" --hostname="${TAILSCALE_HOSTNAME}" --advertise-exit-node "${TAILSCALE_ADDITIONAL_ARGS}"; do
+until /etc/tailscale.d/tailscale up --login-server="${TAILSCALE_LOGIN_SERVER}" --authkey="${TAILSCALE_AUTHKEY}" --hostname="${TAILSCALE_HOSTNAME}" --advertise-exit-node "${TAILSCALE_ADDITIONAL_ARGS}"; do
   sleep 0.1
 done
 
